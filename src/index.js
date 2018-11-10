6e1bd054abb7ce5d96d18dc3608d8dc27b568926
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-10 15:07:50 -0500

    Adding specialized solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+//  CONTAINMENT
 
 function FancyBorder(props) {
     return (
@@ -24,13 +25,33 @@ function WelcomeDialog() {
             */}
             <h1 className="Dialog-title">Welcome</h1>
             <p className="Dialog-message">
-                Thank you for visiting our spacecraft!
+                Thank you for visiting our CONTAINMENT spacecraft!
             </p>
         </FancyBorder>
     );
 }
 
-ReactDOM.render(<WelcomeDialog />, document.getElementById('root'));
+//  SPECIALIZATION
+
+function Dialog(props) {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">{props.title}</h1>
+            <p className="Dialog-message">{props.message}</p>
+        </FancyBorder>
+    );
+}
+
+function WelcomeDialog2(){
+    return (<Dialog title="Welcome" message="Thank you, for visiting SPECIALIZATION spacecraft." />);
+}
+
+const elements =    <div>
+                        <WelcomeDialog />
+                        <WelcomeDialog2 />
+                    </div>
+
+ReactDOM.render(elements, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
