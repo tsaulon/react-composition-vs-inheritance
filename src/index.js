93c9ab14a9d7a648d447a65594cf896e094d977d
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-10 14:59:50 -0500

    Appending solutions for passing child elements to a component.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children /* Use this to reference child elements passed into component*/}    
+        </div>
+    );
+}
+
+function WelcomeDialog() {
+    return (
+        //  Note: JSX Components can be passed to Components by their properties
+        //  Example: <FancyBorder left={LComp} right={RComp} />
+        <FancyBorder color="blue">
+            {/* 
+                props.children from FancyBorder 
+                component references these child 
+                components
+            */}
+            <h1 className="Dialog-title">Welcome</h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
+}
+
+ReactDOM.render(<WelcomeDialog />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
